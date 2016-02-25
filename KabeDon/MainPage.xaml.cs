e46bfc5bdeb81717baa346c552a334439cb504cf
hiromi-matsuo <matsuo@kk-itec.co.jp>
2016-02-25 13:36:36 +0900

    サンプルプロジェクト作成

---
+++ KabeDon/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
+
+namespace KabeDon
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            image.Tapped += Image_Tapped;
+        }
+
+        private void Image_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var imageUri = new Uri("ms-appx:///Assets/Image/購入者特典描き下ろしイラスト（驚き）.png");
+            ShowImage(imageUri);
+        }
+
+        private async void ShowImage(Uri imageUri)
+        {
+            var bitmap = new BitmapImage();
+            var file = await StorageFile.GetFileFromApplicationUriAsync(imageUri);
+            var stream = await file.OpenReadAsync();
+            await bitmap.SetSourceAsync(stream);
+            image.Source = bitmap;
+        }
+    }
+}
